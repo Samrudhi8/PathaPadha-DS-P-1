ecca4252f5e60c096a39f777760d59bc9ff2defe
Samrudhi8 <73016002+Samrudhi8@users.noreply.github.com>
2021-08-31 00:52:35 +0530

    Add files via upload

---
+++ DisBtwPoints.py
@@ -0,0 +1,8 @@
+x1 = int(input("Enter the x coordinate of first point"))
+y1 = int(input("Enter the y coordinate of first point"))
+x2 = int(input("Enter the x coordinate of second point"))
+y2 = int(input("Enter the y coordinate of second point"))
+
+d = ((x2 - x1)**2 + (y2 - y1)**2)**(0.5)
+
+print("Distance between the two points is = ",d)
