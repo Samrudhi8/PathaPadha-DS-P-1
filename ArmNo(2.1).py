a46dcfb5ffc1ab144deb34faa6401befc3db962a
Samrudhi8 <73016002+Samrudhi8@users.noreply.github.com>
2021-08-31 21:08:59 +0530

    Add files via upload

---
+++ ArmNo(2.1).py
@@ -0,0 +1,12 @@
+num = int(input("Enter a Number: "))
+sum = 0
+var = num
+while num > 0:
+    r = var%10
+    sum += r*r*r
+    var //= 10
+
+if num == sum:
+    print("It is an Armstrong Number")
+else:
+    print("It is not an Armstrong Number")
